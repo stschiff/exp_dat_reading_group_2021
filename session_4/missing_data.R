69da8e7710934f590f2b324ab2044f7eeeef8c88
Clemens Schmid <clemens@nevrome.de>
2021-09-30 23:23:43 +0200

    exploring some methods to fill missing values

--- session_4/missing_data.R
+++ session_4/missing_data.R
@@ -1,5 +1,5 @@
 # install dependencies
-packages <- c("tidyverse", "palmerpenguins", "cowplot")
+packages <- c("tidyverse", "palmerpenguins", "cowplot", "softImpute", "missMethods", "norm", "mvtnorm")
 missing_packages <- packages[!packages %in% utils::installed.packages()]
 Map(function(x) { install.packages(x) }, missing_packages)
 
@@ -28,6 +28,8 @@ pnf_matrix <- pengu_na_filtered %>%
   as.matrix()
 
 pnf_pca <- prcomp(pnf_matrix, scale. = T)
+
+####
 
 biplot(pnf_pca)
 
@@ -72,28 +74,67 @@ p0 <- ggplot() +
 # p2 <- cowplot::insert_yaxis_grob(p1, ydens, grid::unit(.2, "null"), position = "right")
 # cowplot::ggdraw(p2)
 
-pnf_tidy_obs_long <- pnf_tidy_obs %>%
-  tidyr::pivot_longer(
-    cols = c(
-      "bill_length_mm", "bill_depth_mm", 
-      "flipper_length_mm", "body_mass_g"
-    ),
-    names_to = "var",
-    values_to = "meas"
-  )
+# pnf_tidy_obs_long <- pnf_tidy_obs %>%
+#   tidyr::pivot_longer(
+#     cols = c(
+#       "bill_length_mm", "bill_depth_mm", 
+#       "flipper_length_mm", "body_mass_g"
+#     ),
+#     names_to = "var",
+#     values_to = "meas"
+#   )
+# 
+# pnf_tidy_obs_long %>%
+#   dplyr::group_split(var) %>%
+#   purrr::map(
+#     ~ggplot() +
+#       geom_point(
+#         data = .,
+#         mapping = aes(x = PC1, y = PC2, colour = meas)
+#       ) +
+#       scale_color_viridis_c() +
+#       ggtitle(unique(.$var))
+#   ) %>%
+#   cowplot::plot_grid(plotlist = .)
 
-pnf_tidy_obs_long %>%
-  dplyr::group_split(var) %>%
-  purrr::map(
-    ~ggplot() +
-      geom_point(
-        data = .,
-        mapping = aes(x = PC1, y = PC2, colour = meas)
-      ) +
-      scale_color_viridis_c() +
-      ggtitle(unique(.$var))
-  ) %>%
-  cowplot::plot_grid(plotlist = .)
+####
 
+pnf_matrix
 
+shoot_holes <- function(x, prop) {
+  nr_cells <- prod(dim(x))
+  holes <- sample(seq_len(nr_cells), size = round(prop * nr_cells))
+  x[holes] <- NA
+  return(x)
+}
 
+pengu_perforated <- shoot_holes(pnf_matrix, 0.2)
+
+patch_holes_mean <- function(x) {
+  apply(x, 2, function(y) { 
+    y[is.na(y)] <- mean(y, na.rm = T)
+    return(y)
+  })
+}
+
+patch_holes_mean(pengu_perforated) %>% 
+  prcomp(scale. = T) %>% biplot()
+
+fits <- softImpute::softImpute(pengu_perforated, type="svd")
+softImpute::complete(pengu_perforated, fits) %>%
+  prcomp(scale. = T) %>% biplot()
+
+missMethods::impute_EM(pengu_perforated) %>% 
+  prcomp(scale. = T) %>% biplot()
+
+missMethods::impute_mean(pengu_perforated) %>% 
+  prcomp(scale. = T) %>% biplot()
+
+missMethods::impute_median(pengu_perforated) %>% 
+  prcomp(scale. = T) %>% biplot()
+
+missMethods::impute_mode(pengu_perforated) %>% 
+  prcomp(scale. = T) %>% biplot()
+
+missMethods::impute_sRHD(pengu_perforated) %>% 
+  prcomp(scale. = T) %>% biplot()
