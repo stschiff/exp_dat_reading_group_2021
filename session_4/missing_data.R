6c15dc5b78235fb2c9489eef2807f42b4cdf7226
Clemens Schmid <clemens@nevrome.de>
2021-10-12 21:27:49 +0200

    running pca exploration with the genotype dataset

--- session_4/missing_data.R
+++ session_4/missing_data.R
@@ -1,70 +1,46 @@
-geno_matrix2 <- scan("geno_matrix.txt", what = "character") %>%
-  strsplit("") %>%
-  do.call(rbind, .) %>%
-  apply(., 2, as.numeric)
-
 # install dependencies
-packages <- c("tidyverse", "palmerpenguins", "cowplot", "softImpute", "missMethods", "norm", "mvtnorm")
-missing_packages <- packages[!packages %in% utils::installed.packages()]
-Map(function(x) { install.packages(x) }, missing_packages)
+packages <- c("tidyverse", "cowplot", "softImpute", "missMethods", "norm", "mvtnorm", "ggrepel")
+Map(function(x) { install.packages(x) }, packages[!packages %in% utils::installed.packages()])
 
 library(magrittr)
 library(ggplot2)
 
-pengu <- palmerpenguins::penguins %>%
-  dplyr::mutate(id = seq_len(nrow(.)) %>% as.character())
+# load data
+geno_matrix <- scan("geno_matrix.txt", what = "character") %>%
+  strsplit("") %>%
+  do.call(rbind, .) %>%
+  apply(., 2, as.numeric)
 
-pnf <- pengu_na_filtered <- pengu %>%
-  dplyr::filter(
-    dplyr::across(
-      tidyselect::any_of(c(
-        "bill_length_mm", "bill_depth_mm", 
-        "flipper_length_mm", "body_mass_g"
-      )),
-      function(x) { !is.na(x) }
-    )
-  )
-  
-pnf_matrix <- pengu_na_filtered %>%
-  dplyr::select(
-    "bill_length_mm", "bill_depth_mm", 
-    "flipper_length_mm", "body_mass_g"
-  ) %>% 
-  as.matrix()
-
-pnf_pca <- prcomp(pnf_matrix, scale. = T)
-#biplot(pnf_pca)
+context_info <- readr::read_csv("context_info.csv")
 
 ####
 
-tidy_pca_output <- function(x, ids = pnf$id, raw_input = pengu) {
+tidy_pca_output <- function(x, context = context_info) {
   pnf_tidy_obs <- x$x %>%
     tibble::as_tibble() %>%
-    dplyr::mutate(
-      id = ids,
-      type = "obs"
-    ) %>%
-    dplyr::left_join(
-      raw_input, by = "id"
-    )
+    dplyr::bind_cols(context) %>%
+    dplyr::mutate(type = "obs", id = Group_Name)
   pnf_tidy_vars <- x$rotation %>%
     tibble::as_tibble(rownames = "id") %>%
-    dplyr::mutate(
-      type = "vars"
-    )
+    dplyr::mutate(type = "vars")
   list(
     obs = pnf_tidy_obs,
     vars = pnf_tidy_vars
   )
 }
 
-pnf_tidy <- tidy_pca_output(pnf_pca, pnf$id, pengu)
-
 plot_tidy_pca_simple <- function(x) {
-  ggplot() + geom_point(
-    data = x$obs,
-    mapping = aes(x = PC1, y = PC2, colour = species)
-  )
+  ggplot() + 
+    geom_point(
+      data = x$obs,
+      mapping = aes(x = PC1, y = PC2, colour = Makro_Region),
+      size = 3
+    ) +
+    ggrepel::geom_text_repel(
+      data = x$obs,
+      mapping = aes(x = PC1, y = PC2, label = Group_Name),
+      size = 3
+    )
 }
 
 plot_tidy_pca_density <- function(x) {
@@ -72,13 +48,13 @@ plot_tidy_pca_density <- function(x) {
   xdens <- cowplot::axis_canvas(p0, axis = "x") +
     geom_density(
       data = x$obs,
-      aes(x = PC1, fill = species),
+      aes(x = PC1, fill = Makro_Region),
       alpha = 0.7
     )
   ydens <- cowplot::axis_canvas(p0, axis = "y", coord_flip = TRUE) +
     geom_density(
       data = x$obs,
-      aes(x = PC2, fill = species),
+      aes(x = PC2, fill = Makro_Region),
       alpha = 0.7
     ) +
     coord_flip()
@@ -86,6 +62,9 @@ plot_tidy_pca_density <- function(x) {
   p2 <- cowplot::insert_yaxis_grob(p1, ydens, grid::unit(.2, "null"), position = "right")
   cowplot::ggdraw(p2)
 }
+
+pca <- prcomp(geno_matrix)
+pnf_tidy <- tidy_pca_output(pca)
 
 plot_tidy_pca_simple(pnf_tidy)
 plot_tidy_pca_density(pnf_tidy)
@@ -122,7 +101,7 @@ shoot_holes <- function(x, prop) {
   return(x)
 }
 
-pengu_perforated <- shoot_holes(pnf_matrix, 0.2)
+geno_perforated <- shoot_holes(geno_matrix, 0.2)
 
 patch_holes_mean <- function(x) {
   apply(x, 2, function(y) { 
@@ -131,8 +110,8 @@ patch_holes_mean <- function(x) {
   })
 }
 
-patch_holes_mean(pengu_perforated) %>% 
-  prcomp(scale. = T) %>% tidy_pca_output() %>% plot_tidy_pca_density()
+patch_holes_mean(geno_perforated) %>% 
+  prcomp() %>% tidy_pca_output() %>% plot_tidy_pca_density()
 
 # fits <- softImpute::softImpute(pengu_perforated, type="svd")
 # pnf_matrix %>% shoot_holes(pnf_matrix, 0.2) %>%
@@ -141,15 +120,15 @@ patch_holes_mean(pengu_perforated) %>%
 
 explore_filling_method <- function(x, f, destruction_level) {
   x %>% shoot_holes(destruction_level) %>% f() %>%
-    prcomp(scale. = T) %>% tidy_pca_output() %>% plot_tidy_pca_density()
+    prcomp() %>% tidy_pca_output() %>% plot_tidy_pca_density()
 }
 
-explore_filling_method(pnf_matrix, missMethods::impute_mean, 0.5)
-explore_filling_method(pnf_matrix, missMethods::impute_median, 0.5)
-explore_filling_method(pnf_matrix, missMethods::impute_mode, 0.2)
+explore_filling_method(geno_matrix, missMethods::impute_mean, 0.2)
+explore_filling_method(geno_matrix, missMethods::impute_median, 0.2)
+explore_filling_method(geno_matrix, missMethods::impute_mode, 0.2)
 
-explore_filling_method(pnf_matrix, missMethods::impute_EM, 0.5)
-explore_filling_method(pnf_matrix, missMethods::impute_sRHD, 0.5)
+explore_filling_method(geno_matrix, missMethods::impute_EM, 0.2)
+explore_filling_method(geno_matrix, missMethods::impute_sRHD, 0.2)
 
 # Mean per penguin species would be much better, probably
 
